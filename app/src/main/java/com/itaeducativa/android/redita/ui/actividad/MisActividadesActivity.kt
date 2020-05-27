0d9eb73793db9491450c4946b3eabcd18611f619
andresFMedina <andres.medina@correounivalle.edu.co>
2020-05-27 16:06:13 -0500

    Se agregan las interfaces gráficas de las actividades

---
+++ app/src/main/java/com/itaeducativa/android/redita/ui/actividad/MisActividadesActivity.kt
@@ -0,0 +1,13 @@
+package com.itaeducativa.android.redita.ui.actividad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itaeducativa.android.redita.R
+
+class MisActividadesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mis_actividades)
+    }
+}
