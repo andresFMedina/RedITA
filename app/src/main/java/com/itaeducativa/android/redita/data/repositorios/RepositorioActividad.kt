8e76bc8a69b2ddd38fd55caea8ada13dc04f34b1
oscarruiz15 <oarp1996@hotmail.com>
2020-07-16 22:13:29 -0500

    Debug eliminar actividad/reto y alert dialogo

--- app/src/main/java/com/itaeducativa/android/redita/data/repositorios/RepositorioActividad.kt
+++ app/src/main/java/com/itaeducativa/android/redita/data/repositorios/RepositorioActividad.kt
@@ -1,7 +1,10 @@
 package com.itaeducativa.android.redita.data.repositorios
 
 import com.google.android.gms.tasks.Task
-import com.google.firebase.firestore.*
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.itaeducativa.android.redita.data.firebase.FirebaseSource
 import com.itaeducativa.android.redita.data.modelos.Actividad
 
@@ -88,7 +91,7 @@ class RepositorioActividad(private val firebase: FirebaseSource) {
     }
 
     fun desactivarActividad(actividad: Actividad): Task<Void> =
-        firestoreDB.collection(ACTIVIDADES).document(actividad.fechaCreacionTimeStamp)
+        firestoreDB.collection(ACTIVIDADES).document(actividad.id)
             .update("estaActivo", false)
 
     fun eliminarNombre(actividadId: String): Task<Void> =
