a6b10c7bd63ef7087526cc613ad167dc676725b9
andresFMedina <andres.medina@correounivalle.edu.co>
2020-07-25 14:37:10 -0500

    Arreglitos x2

--- app/src/main/java/com/itaeducativa/android/redita/ui/actividad/ui/FormularioActividadActivity.kt
+++ app/src/main/java/com/itaeducativa/android/redita/ui/actividad/ui/FormularioActividadActivity.kt
@@ -155,6 +155,7 @@ class CrearActividadActivity : AppCompatActivity(), RequestListener, KodeinAware
             mensaje,
             coordinatorCrearActividad
         )
+        finish()
     }
 
     override fun onFailureRequest(message: String) {
